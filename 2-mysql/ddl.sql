ccb0ebdb603d00757b319bb4493ed8b720791da7
Ruslan Kladko <rkladko@gmail.com>
2016-11-25 20:32:08 +0200

    Correct join type

--- 2-mysql/ddl.sql
+++ 2-mysql/ddl.sql
@@ -4,5 +4,5 @@ create index link_info_id on link(info_id);
 SELECT *
 FROM
   link l
-    left join data d on d.id = l.data_id
-    left join info i on i.id = l.info_id;
+    inner join data d on d.id = l.data_id
+    inner join info i on i.id = l.info_id;
